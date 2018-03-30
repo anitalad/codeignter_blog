5a48ea5296944cdb34f5c90da4dc0700de96c1f8
Anita Lad <jagtapanita@gmail.com>
2018-03-30 22:34:01 +0530

    Initial commit with base files

---
+++ application/controllers/User.php
@@ -0,0 +1,10 @@
+<?php
+
+class User extends MY_Controller {
+
+	public function index() {
+		echo "List public article";
+	}
+}
+
+?>
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class MY_controller extends CI_Controller {
+
+	public function __construct() {
+		//if( !$this->isAutorized() ) return redirect('home');
+	}
+
+}
+
+?>
